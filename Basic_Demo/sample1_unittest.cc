05287f57f8be1a362b45079621de2865fcd3dcd6
ywolf-hu <hlf0312@163.com>
2015-02-24 23:24:05 +0800

    Revert misoperation of sample_unittest.cc

--- Basic_Demo/sample1_unittest.cc
+++ Basic_Demo/sample1_unittest.cc
@@ -79,7 +79,7 @@
 TEST(FactorialTest, Negative) {
   // This test is named "Negative", and belongs to the "FactorialTest"
   // test case.
-  EXPECT_EQ(2, Factorial(-5));
+  EXPECT_EQ(1, Factorial(-5));
   EXPECT_EQ(1, Factorial(-1));
   EXPECT_GT(Factorial(-10), 0);
 
